aaaca05f3445ca6b500e9797e8347edeb32a3973
youn2029 <cocoa8090@gmail.com>
2019-04-11 13:42:42 +0900

    맵 표시 코드를 클래스로 분리

--- Custom-Alert/MapAlertViewController.swift
+++ Custom-Alert/MapAlertViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import MapKit
 
 class MapAlertViewController: UIViewController {
     
@@ -34,40 +33,12 @@ class MapAlertViewController: UIViewController {
         // 알림창
         let alert = UIAlertController(title: nil, message: "여기가 맞습니까?", preferredStyle: .alert)
         
-        //////////////////////////////////////////////////////////////////////////////////////
-        
-        // 콘텐츠 뷰
-        let contentVC = UIViewController()
-        
-        // 뷰 컨트롤러에 맵킨 뷰를 추가
-        let mapkitView = MKMapView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        contentVC.view = mapkitView
-        
-        // 해당 위치 (위도/경도로 지정)
-        let pos = CLLocationCoordinate2D(latitude: 37.514322, longitude: 126.894623)
-        
-        // 지도에서 보여줄 넓이 (축척) : 숫자가 작을수록 좁은 범위를 확대시켜서 보여줌
-        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
-        
-        // 지도 영역을 정의
-        let region = MKCoordinateRegion(center: pos, span: span)
-        
-        // 지도 뷰에 표시
-        mapkitView.region = region
-        mapkitView.regionThatFits(region)
-        
-        // 해당 위치를 지정할 핀
-        let point = MKPointAnnotation()
-        point.coordinate = pos
-        point.title = "여기!!"
-        point.subtitle = "맞아요?"
-        mapkitView.addAnnotation(point)
-        
-        //////////////////////////////////////////////////////////////////////////////////////
-        
         // 버튼
         let ok = UIAlertAction(title: "OK", style: .default)
         let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        // 콘텐츠 뷰 컨트롤러
+        let contentVC = MapKitViewController()
         
         // 알림창에 추가
         alert.setValue(contentVC, forKey: "contentViewController")
---
+++ Custom-Alert/MapKitViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MapKitViewController.swift
+//  Custom-Alert
+//
+//  Created by 윤성호 on 11/04/2019.
+//  Copyright © 2019 윤성호. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapKitViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 뷰 컨트롤러에 맵킨 뷰를 추가
+        let mapkitView = MKMapView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        self.view = mapkitView
+        self.preferredContentSize.height = 200
+        
+        // 해당 위치 (위도/경도로 지정)
+        let pos = CLLocationCoordinate2D(latitude: 37.514322, longitude: 126.894623)
+        
+        // 지도에서 보여줄 넓이 (축척) : 숫자가 작을수록 좁은 범위를 확대시켜서 보여줌
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        
+        // 지도 영역을 정의
+        let region = MKCoordinateRegion(center: pos, span: span)
+        
+        // 지도 뷰에 표시
+        mapkitView.region = region
+        mapkitView.regionThatFits(region)
+        
+        // 해당 위치를 지정할 핀
+        let point = MKPointAnnotation()
+        point.coordinate = pos
+        point.title = "여기!!"
+        point.subtitle = "맞아요?"
+        mapkitView.addAnnotation(point)
+    }
+}
